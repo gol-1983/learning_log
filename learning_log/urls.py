db8cdc98ff9e2609c600b93f6630a9b0bbfe8c8a
owner <oleg.grodskiy@gmail.com>
2022-09-19 12:34:25 +0100

    Ready for deployment to heroku.

---
+++ learning_log/urls.py
@@ -0,0 +1,11 @@
+"""Defines url patterns for learning_logs."""
+
+from django.urls import path, include
+from django.contrib import admin
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('users/', include('users.urls')),
+    path('', include('learning_logs.urls')),
+]
